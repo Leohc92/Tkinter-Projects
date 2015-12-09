3eaa9798074843657c85141257805ebfbe158e72
Wenice <chenyu.wu@outlook.com>
2015-12-09 10:31:10 +0800

    modified chessboard and pieces files

--- 03-TkChess/chessboard.py
+++ 03-TkChess/chessboard.py
@@ -1,1 +1,48 @@
 #coding:utf8
+
+import pieces
+import re
+
+START_PATTERN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w 0 1'
+
+class Board(dict):
+	y_axis = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
+	x_axis = (1, 2, 3, 4, 5, 6, 7, 8)
+
+	def __init__(self, patt = None):
+		self.process_notation(START_PATTERN)
+
+	def alpha_notation(self, xycoord):
+		if not self.is_on_board(xycoord):return 
+		return self.y_axis[xycoord[1]] + str(self.x_axis[xycoord[0]])
+
+	def num_notation(self, xycoord):
+		return int(xycoord[1])-1, self.y_axis.index(xycoord[0])
+
+	def process_notation(self, patt):
+		patt = patt.split(' ')
+		def expand_whitespaces(match): return ' '*int(match.group(0))
+		patt[0] = re.compile(r'\d').sub(expand_whitespaces, patt[0])
+		for x, row in enumerate(patt[0].split('/')):
+			for y, alphabet in enumerate(row):
+				if alphabet == ' ': continue
+				xycoord = self.alpha_notation((7-x, y))
+				self[xycoord] = pieces.create_piece(alphabet)
+				self[xycoord].ref(self)
+		if patt[1] == 'w': self.player_turn = 'white'
+		else: 
+			self.player_turn = 'black'
+
+	def occupied(self, color):
+		result=[]
+		for coord in self:
+			if self[coord].color == color:
+				result.append(coord)
+			return result
+
+	def is_on_board(self, coord):
+		if coord[1]<0 or corrd[1]>7 or coord[0]<0 or coord[0]>7:
+			return False
+		else: return True
+
+class ChessError(Exception): pass
--- 03-TkChess/pieces.py
+++ 03-TkChess/pieces.py
@@ -1,1 +1,31 @@
 #coding:utf8
+
+import sys
+
+SHORT_NAME = {'R':'Rook', 'N':'Knight', 'B':'Bishop', 'Q':'Queen', 'K':'King', 'P':'Pawn'}
+
+def create_piece(piece, color='white'):
+	if piece in (None, ' '): return
+	if piece.isupper(): color = 'white'
+	else:	color = 'black'
+	piece = SHORT_NAME[piece.upper()]
+	module = sys.modules[__name__]
+	return module.__dict__[piece](color)
+
+class Piece():
+	def __init__(self, color):
+		if color == 'black':
+			self.shortname = self.shortname.lower()
+		elif color == 'white':
+			self.shortname = self.shortname.upper()
+		self.color = color
+
+	def ref(self, board):
+		self.board = board
+
+class King(Piece): shortname = 'k'
+class Queen(Piece): shortname = 'q'
+class Rook(Piece): shortname ='r'
+class Knight(Piece): shortname = 'n'
+class Bishop(Piece): shortname = 'b'
+class Pawn(Piece): shortname = 'p'
